eba7d201ed9355d3567a2160f9c0c9bcd7820c40
sayem499 <sayem499@gmail.com>
2023-10-26 22:07:18 +0600

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: /* 'http://host.docker.internal:5000' ||  */'http://localhost:5000',
+      target: /* 'http://host.docker.internal:5000' || 'http://localhost:5000'*/ 'https://pos-app-backend.onrender.com',
       changeOrigin: true,
     })
   );
