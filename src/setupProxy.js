e48f183bb7524ac54b57e683cafd90df7290dc97
sayem499 <sayem499@gmail.com>
2023-10-26 21:58:32 +0600

    package.json update

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://host.docker.internal:5000' || 'http://localhost:5000',
+      target: /* 'http://host.docker.internal:5000' ||  */'http://localhost:5000',
       changeOrigin: true,
     })
   );
