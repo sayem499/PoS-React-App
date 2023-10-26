bce57a540d964132a85287d3785530bc9d99919f
sayem499 <sayem499@gmail.com>
2023-10-26 14:49:49 +0600

    Update for deploy.

--- pos-app/backend/config/db.js
+++ pos-app/backend/config/db.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 const mongoose = require('mongoose')
+
+
 
 
 const connectDB = async () => {
     try {
         const uri = process.env.ATLAS_URI
         const conn = await mongoose.connect(uri, {dbName:'PoSReactApp', useNewUrlParser: true }) 
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log(error)
-        process.exit(1)
-
+        console.error(`Error : ${error.message}`);
+        process.exit(1);
     }
 }
 
--- pos-app/backend/server.js
+++ pos-app/backend/server.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+const path = require('path');
 const express = require('express')
 const cors = require('cors')
 const {errorHandler} = require('./middleware/errorMiddleware.js')
 const connectDB = require('./config/db.js')
-require('dotenv').config();
+
 
 const app = express()
-const port = process.env.PORT || 5000
+const port = process.env.SERVER_PORT || 5000
+
+
 
 connectDB()
 
@@ -17,6 +21,16 @@ app.use('/api/products', require('./routes/product.routes'))
 app.use('/api/users', require('./routes/user.routes'))
 app.use('/api/customers', require('./routes/customer.routes'))
 app.use('/api/suppliers', require('./routes/supplier.routes'))
+
+
+if(process.env.NODE_ENV === "production"){
+    const _rootdire = path.resolve()
+    app.use(express.static(path.join(_rootdire, '/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(_rootdire, 'build', 'index.html')))
+}else{
+    app.get('/', (req, res) => res.send('Server is ready.'))
+}
+
 
 
 app.use(errorHandler)
